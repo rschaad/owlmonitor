7e56c9e76c8123ee43ea11a841dba72e5d74e683
Roger Schaad <rschaad@netatc.ch>
2014-09-08 20:50:36 +0200

    First working sample. Simply listening to the UDP messages

---
+++ index.js
@@ -0,0 +1,32 @@
+
+var util = require('util');
+var OWL = require('owlintuition');
+
+var owl = new OWL();
+owl.monitor();
+
+// Event Messages --------------------------------------------------------
+
+owl.on('connect', function( event ) {
+	console.log( "connect" );
+});
+
+owl.on('electricity', function( event ) {
+	console.log( "electricity = " + util.inspect(event, {"depth": null}) );
+});
+
+owl.on('heating', function( event ) {
+	console.log( "heating = " + util.inspect( event, {"depth": null}) );
+});
+
+owl.on('weather', function( event ) {
+	console.log( "weather = " + util.inspect( event, {"depth": null}) );
+});
+
+owl.on('solar', function( event ) {
+	console.log( "solar = " + util.inspect(event, {"depth": null}) );
+});
+
+owl.on('disconnect', function( event ) {
+	console.log( "disconnect" );
+});
